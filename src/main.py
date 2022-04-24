4a848ff4dec552dfa2f46708d257305be340bd3d
gregori0o <glegeza@wp.pl>
2022-04-24 21:55:25 +0200

    fix: Fix bugs and visualize each line.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from base_structure import Graph
 from initial_solution import CreateSolution
 from json_parser import Parser
+from src.visualization import GraphVisualizer, LinesVisualizer, SolutionVisualizer
 
 
 def main():
-    _parser = Parser("../utils/graphs/g1.json")
+    _parser = Parser("utils/graphs/g1.json")
     interchange_points = _parser.get_interchange_points()
     size = _parser.get_size()
     edges = _parser.get_edges()
@@ -15,6 +16,16 @@ def main():
     init = CreateSolution(graph)
     lines, buses = init.create_init_solution(3, 20)
     print(size, edges, interchange_points, lines, buses)
+
+    graph_vis = GraphVisualizer(size, edges)
+    graph_vis.save('graph')
+
+    for i, line in enumerate(lines):
+        lines_vis = LinesVisualizer(size, edges, [line], interchange_points)
+        lines_vis.save('line{}'.format(i))
+
+    # solution_vis = SolutionVisualizer(size, edges, lines, interchange_points)
+    # solution_vis.save('test')
 
 
 if __name__ == '__main__':
--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,7 @@
 import graphviz
 import random
 
+
 # colors:
 # https://graphviz.org/doc/info/colors.html
 def create_colors_for_lines(num_of_lines: int):
@@ -8,6 +9,7 @@ def create_colors_for_lines(num_of_lines: int):
     r = lambda: random.randint(150, 255)
     colors = ['#%02X%02X%02X' % (r(), r(), r()) for line in range(num_of_lines)]
     return colors
+
 
 class GraphVisualizer:
     def __init__(self, size: int, edges: tuple[any, any, int]):
@@ -29,14 +31,16 @@ class GraphVisualizer:
         return self.graph
 
     def save(self, filename: str):
-        self.graph.view(filename=filename, directory='../utils/visualizations/graphs')
+        self.graph.view(filename=filename, directory='utils/visualizations/graphs')
 
 
 class LinesVisualizer:
-    def __init__(self, size: int, edges: tuple[any, any, int], lines: tuple, stations: tuple):
+    def __init__(self, size: int, edges: tuple[any, any, int], lines: list[list[int]], stations: tuple):
         self.colors = create_colors_for_lines(len(lines))
         self.graph = graphviz.Graph('Buses lines', engine='sfdp')
+        self.graph.attr(label=('\n').join(['{} -> {}'.format(i, line) for i, line in enumerate(lines)]))
         self.add_interchange_stations(stations)
+        self.add_edges(edges)
         self.add_lines(lines, edges)
 
     def add_interchange_stations(self, stations):
@@ -54,8 +58,16 @@ class LinesVisualizer:
                 i += 1
                 j += 1
 
+    def add_edges(self, edges):
+        for x, y, val in edges:
+            self.graph.attr('edge', penwidth='1')
+            self.graph.edge('#' + str(x), '#' + str(y), label=str(val))
+
     def show(self):
         return self.graph
+
+    def save(self, filename: str):
+        self.graph.view(filename=filename, directory='utils/visualizations/lines')
 
 
 class SolutionVisualizer:
@@ -90,42 +102,5 @@ class SolutionVisualizer:
         return self.graph
 
     def save(self, filename: str):
-        self.graph.view(filename=filename, directory='../utils/visualizations/buses_lines')
+        self.graph.view(filename=filename, directory='utils/visualizations/buses_lines')
 
-g = graphviz.Graph('Buses lines', filename='../utils/visualizations/buses_lines/buses_lines.gv', engine='sfdp')
-
-# creates random colors for edges
-NUM_OF_LINES = 4
-r = lambda: random.randint(150, 255)
-colors = ['#%02X%02X%02X' % (r(),r(),r()) for line in range(NUM_OF_LINES)]
-# print(colors)
-
-# adds transfer stations
-g.attr('node', shape='doubleoctagon', style='filled', fillcolor='lightcoral', fixedsize='true')
-g.node('#2')
-g.node('#6')
-g.node('#7')
-g.node('#8')
-
-
-# adds unattended line segments
-g.attr('node', shape='circle', style='filled', fillcolor='lightskyblue1')
-g.attr('edge', penwidth='1')
-g.edge('#2', '#3', label='3')
-g.edge('#6', '#8', label='3')
-
-# adds attended line segments
-g.attr('edge', penwidth='5')
-g.edge('#1', '#2', label='3', color=colors[0])
-g.edge('#2', '#4', label='2', color=colors[0])
-g.edge('#4', '#5', label='1', color=colors[1])
-g.edge('#5', '#8', label='1', color=colors[1])
-g.edge('#3', '#6', label='5', color=colors[2])
-g.edge('#6', '#7', label='6', color=colors[2])
-g.edge('#7', '#10', label='2', color=colors[2])
-g.edge('#7', '#8', label='3', color=colors[3])
-g.edge('#8', '#9', label='5', color=colors[3])
-g.edge('#9', '#10', label='4', color=colors[3])
-
-# creates pdf file
-# g.view()
