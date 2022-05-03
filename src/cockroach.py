ea49914d6946b50672b1e451ddf7f499ca0ea4f9
rysiek <r.precikowski@gmail.com>
2022-05-03 17:19:19 +0200

    chase swarming somewhat working

--- src/cockroach.py
+++ src/cockroach.py
@@ -1,30 +1,33 @@
 import random
 
 from base_structure import Graph
-from graph_generator import GraphGenerator
 from initial_solution import CreateSolution
 from json_parser import Parser
 
-from pprint import pprint
-
 
 class Cockroach:
-    def __init__(self, lines):
+    __id = 0
+    min_common = 9
+    step_size = 2
+
+    def __init__(self, lines: list):
         self.visible_cockroaches: list[Cockroach] = []
 
-        self.min_common = 8
-
         self.lines = lines
-        self.edges = self.__update_edges()
+        self.edges = self.__update_edges()  # edge: no_line
         self.metric_value = self.__evaluate_metric_value()
 
-    def __update_edges(self):
-        self.edges: set[tuple[int, int]] = set()
+        self.id = Cockroach.__id
+        Cockroach.__id += 1
 
-        for line in self.lines:
-            for v, u in zip(line[:-1], line[1:]):
+    def __update_edges(self):
+        self.edges: dict[tuple[int, int], (int, int)] = {}
+
+        for i, line in enumerate(self.lines):
+            for edge_no, tmp in enumerate(zip(line[:-1], line[1:])):
+                v, u = tmp
                 e = v[0], u[0]
-                self.edges.add(e)
+                self.edges[e] = i, edge_no
         return self.edges
 
     def update_visible_cockroaches(self, all_cockroaches):
@@ -34,9 +37,9 @@ class Cockroach:
         """
 
         def check_if_visible(cockroach: Cockroach):
-            return len(self.edges & cockroach.edges) >= self.min_common
+            return len(self.edges & cockroach.edges.keys()) >= self.min_common
 
-        self.visible_cockroaches = [c for c in all_cockroaches if c is not self and check_if_visible(c)]
+        self.visible_cockroaches = [c for c in all_cockroaches if c.id != self.id and check_if_visible(c)]
 
     def get_best_visible_cockroach(self):
         def get_metric_value(cockroach):
@@ -54,41 +57,64 @@ class Cockroach:
         return self.metric_value
 
     def get_random_common_edge(self, best_visible):
-        return random.choice(tuple(self.edges & best_visible.edges))
+        return random.choice(tuple(self.edges.keys() & best_visible.edges.keys()))
 
 
-def cockroach_solution(graph):
-    pprint([(v.index, [(u.index, w) for u, w in v.neighbours]) for v in graph.vertices])
-    print()
-    init = CreateSolution(graph)
-    num_cockroaches = 5
+def is_line_better(old, new):
+    # todo
+    desired_len = 5
 
-    cockroaches = []
+    return abs(len(new) - desired_len) <= abs(len(old) - desired_len)
 
-    for i in range(num_cockroaches):
-        lines, buses = init.create_init_solution(3, 20)
 
-        for l in lines:
-            print(l)
-        print("\n")
+class Cockroach_solution:
+    def __init__(self, graph, num_cockroaches=10, num_lines=5):
+        self.solution_creator = CreateSolution(graph)
 
-        cockroaches.append(Cockroach(lines))
+        self.cockroaches = []
 
-    for cockroach in cockroaches:
-        cockroach.update_visible_cockroaches(cockroaches)
+        for i in range(num_cockroaches):
+            lines, buses = self.solution_creator.create_init_solution(num_lines, 20)
+            self.cockroaches.append(Cockroach(lines))
 
-        if len(cockroach.visible_cockroaches) == 0:
-            print("EMPTY")
-            continue
+    def chase_swarming(self):
+        for cockroach in self.cockroaches:
+            cockroach.update_visible_cockroaches(self.cockroaches)
 
-        best_visible = cockroach.get_best_visible_cockroach()
+            if len(cockroach.visible_cockroaches) == 0:
+                print("EMPTY")
+                continue
 
-        random_edge = cockroach.get_random_common_edge(best_visible)
+            best_visible = cockroach.get_best_visible_cockroach()
 
-        print(random_edge)
+            random_edge = cockroach.get_random_common_edge(best_visible)
+            line_no, edge_no = cockroach.edges[random_edge]
+            best_line_no, best_edge_no = best_visible.edges[random_edge]
+            # print(f"{cockroach.id}: edge {random_edge} line_no {line_no} \t best {best_visible.id} line_no {best_line_no}")
 
-        # todo
+            line = cockroach.lines[line_no]
+            best_line = best_visible.lines[best_line_no]
 
+            new_line_start_stops = [l[0] for l in line[:edge_no + 1]]
+            new_line_from_best_stops = [l[0] for l in
+                                        random.sample(best_line[best_edge_no + 1:],
+                                                      Cockroach.step_size if Cockroach.step_size <= len(
+                                                          best_line[best_edge_no + 1:])
+                                                      else len(best_line[
+                                                               best_edge_no + 1:]))]  # todo save order... or no?
+            # print(f"starting {new_line_start_stops} added {new_line_from_best_stops}")
+
+            final_stops = new_line_start_stops + new_line_from_best_stops
+            final_stops += [line[-1][0]] if line[-1][0] not in new_line_from_best_stops else []
+            # print(f"final stops { final_stops }")
+
+            new_line = self.solution_creator.make_lines(final_stops)
+
+            # print(f"{line}\n{best_line}")
+            # print(f"\t{new_line}\n")
+
+            if is_line_better(line, new_line):
+                cockroach.lines[line_no] = new_line
 
 
 def main():
@@ -103,7 +129,9 @@ def main():
         graph.add_edge(i, j, w)
     graph.set_interchange_points(interchange_points)
 
-    cockroach_solution(graph)
+    solution = Cockroach_solution(graph)
+
+    solution.chase_swarming()
 
 
 if __name__ == '__main__':
