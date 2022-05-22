894233292a25214555d191f39d4f88e26fd8804b
gregori0o <glegeza@wp.pl>
2022-05-22 21:11:33 +0200

    update config

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 config = {
-    'num_lines': 3,
-    'num_buses': 20,
+    'num_lines': 4,
+    'num_buses': 50,
     'num_tests': 4, # number for option -t
     'generate': {
-        'vertices': 15,
+        'vertices': 20,
         'min_edges': 30,
         'num_passengers': 80
     },
@@ -12,15 +12,15 @@ config = {
         'min_common': 8,
         'step_size': 2,
         'dispersing_update_ratio': .5,
-        'n_iterations': 5,
+        'n_iterations': 20,
         'num_to_test': 5
     },
 
     'bees': {
         'num_bees': 10,
         'num_transition': 2,
-        'update_ratio': .3,
-        'n_iterations': 5
+        'update_ratio': .4,
+        'n_iterations': 20
     }
 }
 
@@ -32,7 +32,32 @@ criterias = {
     },
     '2': {
         'cockroach': {
-            'n_iterations': [1, 2],
+            'min_common': [1, 2],
+        },
+    },
+    '3': {
+        'cockroach': {
+            'step_size': [1, 2],
+        },
+    },
+    '4': {
+        'cockroach': {
+            'dispersing_update_ratio': [1, 2],
+        },
+    },
+    '5': {
+        'bees': {
+            'num_bees': [5, 10, 15, 20],
+        },
+    },
+    '6': {
+        'bees': {
+            'num_transition': [1, 2, 3, 4],
+        },
+    },
+    '7': {
+        'bees': {
+            'update_ratio': [.3, .4, .5, .6],
         },
     }
 }
